428cc92dc5dac4341c5e18203f7c816ae1e5ab17
patata-GT <gabriele.marchi8@gmail.com>
2021-05-25 14:14:12 +0200

    back-end 1.0

---
+++ back-end 1.0/biblioteca-web-autorizzazione/src/main/java/com/primas/javaee/bibliotecafinale/BibliotecaFinaleApplication.java
@@ -0,0 +1,13 @@
+package com.primas.javaee.bibliotecafinale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaFinaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaFinaleApplication.class, args);
+	}
+
+}
---
+++ back-end 1.0/biblioteca-web-autorizzazione/src/main/java/com/primas/javaee/bibliotecafinale/controllers/AuthController.java
@@ -0,0 +1,127 @@
+package com.primas.javaee.bibliotecafinale.controllers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.primas.javaee.bibliotecafinale.models.ERole;
+import com.primas.javaee.bibliotecafinale.models.Role;
+import com.primas.javaee.bibliotecafinale.models.User;
+import com.primas.javaee.bibliotecafinale.playload.JwtResponse;
+import com.primas.javaee.bibliotecafinale.playload.LoginRequest;
+import com.primas.javaee.bibliotecafinale.playload.MessageResponse;
+import com.primas.javaee.bibliotecafinale.playload.SignupRequest;
+import com.primas.javaee.bibliotecafinale.jwt.JwtUtils;
+import com.primas.javaee.bibliotecafinale.repository.RoleRepository;
+import com.primas.javaee.bibliotecafinale.repository.UserRepository;
+import com.primas.javaee.bibliotecafinale.services.UserDetailsImpl;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/autorizzazione")
+public class AuthController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	RoleRepository roleRepository;
+
+	@Autowired
+	PasswordEncoder encoder;
+
+	@Autowired
+	JwtUtils jwtUtils;
+
+	@PostMapping("/accesso")
+	public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
+
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();		
+		List<String> roles = userDetails.getAuthorities().stream()
+				.map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+
+		return ResponseEntity.ok(new JwtResponse(jwt, 
+												 userDetails.getId(), 
+												 userDetails.getNome(),
+												 userDetails.getCognome(),
+												 userDetails.getEmail(), 
+												 roles));
+	}
+
+	@PostMapping("/registrazione")
+	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+		/*
+		if (userRepository.existsByUsername(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Username is already taken!"));
+		}
+*/
+		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Email is already in use!"));
+		}
+
+		// Create new user's account
+		User user = new User(signUpRequest.getNome(),
+							 signUpRequest.getCognome(),
+							 signUpRequest.getEmail(),
+							 encoder.encode(signUpRequest.getPassword()));
+
+		Set<String> strRoles = signUpRequest.getRole();
+		Set<Role> roles = new HashSet<>();
+
+		if (strRoles == null) {
+			Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+			roles.add(userRole);
+		} else {
+			strRoles.forEach(role -> {
+				switch (role) {
+				case "admin":
+					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(adminRole);
+
+					break;
+	
+				default:
+					Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+					roles.add(userRole);
+				}
+			});
+		}
+
+		user.setRoles(roles);
+		userRepository.save(user);
+
+		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	}
+}
